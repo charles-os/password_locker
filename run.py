b0283d7af36907a4d0101e9b424105be687d503c
chaloo56 <chaloo56@gmail.com>
2019-07-22 11:01:46 +0300

    gave a provission for a new user to create accounts

--- run.py
+++ run.py
@@ -7,3 +7,9 @@ def create_login(firstname, lastname, password):
     """
     new_user = User(firstname,lastname,password)
     return new_user
+def add_account(accname,username,password):
+    """
+    Function to add a new account and its credentials
+    """
+    new_credentials = Credentials(accname,username,password)
+    return new_credentials
